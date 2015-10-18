d4fbbf49f87a7fc5aaf2dd7ae76d9e22e4aeefd6
Adrian Wong <adrianw1832@hotmail.co.uk>
2015-10-19 00:16:01 +0100

    added feature test for creating a restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -18,4 +18,15 @@ feature 'restaurants' do
       expect(page).not_to have_content 'No restaurants yet'
     end
   end
+
+  context 'creating restaurants' do
+    scenario 'prompts user to fill in a form, then displays the restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Fat Duck'
+      click_button 'Create Restaurant'
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content 'Fat Duck'
+    end
+  end
 end
