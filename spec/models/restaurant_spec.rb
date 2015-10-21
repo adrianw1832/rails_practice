a4764f8328c2527aa56dfc6cc49a3691a58f301a
Adrian Wong <adrianwong1832@gmail.com>
2015-10-21 13:59:31 +0100

    implemented rating and uniqueness validation on restaurant and review models

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -8,4 +8,10 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).to be_invalid
   end
+
+  it 'is invalid if the name is not unique' do
+    Restaurant.create(name: 'Fat Duck')
+    restaurant = Restaurant.new(name: 'Fat Duck')
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
